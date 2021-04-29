efca4e0a50a8145b854c2020d0c2ad0f78c8c575
Floris75 <floris.decouflet@gmail.com>
2021-04-29 11:52:00 +0200

    user story liste des villes

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,6 +15,7 @@ router.post("/places", placesController.createOne);
 router.get("/places", placesController.filterPlace);
 router.post("/bookings", bookingsController.bookFlat);
 router.patch("/places/:place_id", placesController.updatePlace);
+router.get("/cities", controller.getCities)
 
 router.use("*", (request, response) => {
     response.status(404).json({message: "La ressource demandée est introuvable"});
